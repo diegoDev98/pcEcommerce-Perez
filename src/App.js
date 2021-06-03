17a31bf1887bc6bb14e55d4f9699eb988aaf654d
diegoDev98 <diegopd98@hotmail.com>
2021-06-03 17:32:35 -0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.png';
 import './App.css';
+import './components/NavBar'
 
 function App() {
   return (
     <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
           Diego's PC Component Store
         </p>
