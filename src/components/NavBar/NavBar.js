33bb2b03df1ffb29fceb2a7950718db0edd6d49b
DiegoDev <83824888+diegoDev98@users.noreply.github.com>
2021-07-06 14:29:55 -0300

    Changed NavLink

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -3,12 +3,6 @@ import CartWidget from './CartWidget'
 import './NavBar.css'
 import {NavLink} from 'react-router-dom'
 
-const categories = [
-  {id:1, category: 'Home'},
-  {id:2, category: 'Pc Parts'},
-  {id:3, category: 'Games'},
-  {id:4, category: 'Monitors'}
-  ]
 
 function NavBar() {
     
@@ -19,9 +13,12 @@ function NavBar() {
                   <NavLink className="navbar-brand" to="/">DiegoTech</NavLink>
                 </div>
                 <ul className="nav navbar-nav">
-                {categories.map( (category,index) =>
-                  <li><NavLink to={`/category/${category.category.toLowerCase().replace(/\s/g, '')}`} key={index} activeClassName="activeLink">{category.category}</NavLink></li>
-												)}
+                
+                  <li><NavLink to={`/`}  >Home</NavLink></li>
+                  <li><NavLink to={`/categories/pcparts`} activeClassName="activeLink">Pc Parts</NavLink></li>
+                  <li><NavLink to={`/categories/games`} activeClassName="activeLink">Games</NavLink></li>
+                  <li><NavLink to={`/categories/monitors`} activeClassName="activeLink">Monitors</NavLink></li>
+											
                  
                   
                 </ul>
